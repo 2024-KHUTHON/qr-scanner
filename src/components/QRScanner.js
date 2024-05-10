077d1f40a98496869ac41c37f693468d7e5b5c27
komg00 <komg00@naver.com>
2024-05-10 22:00:19 +0900

    Feat: 복호화 코드 추가

--- src/components/QRScanner.js
+++ src/components/QRScanner.js
@@ -2,10 +2,12 @@ import React from "react";
 import QrScanner from "qr-scanner";
 import { useEffect, useRef, useState } from "react";
 import "../index.css";
+import axios from "axios";
 
 const QRScanner = (props) => {
   const videoElementRef = useRef(null);
   const [userHash, setUserHash] = useState("");
+  const [userId, setUserId] = useState("");
 
   useEffect(() => {
     const video = videoElementRef.current;
@@ -28,15 +30,36 @@ const QRScanner = (props) => {
       console.log(qrScanner);
       qrScanner.stop();
       qrScanner.destroy();
+      handleDecodeHash();
     };
   }, []);
+
+  const handleDecodeHash = async () => {
+    try {
+      const endpoint = "http://moomu.iptime.org:8855/qr/read";
+
+      const requestBody = {
+        qr_hash: userHash,
+      };
+
+      const response = await axios.get(endpoint, requestBody, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log(response.data.user_id);
+      setUserId(response.data.user_id);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
       <div className="videoWrapper">
         <video className="qrVideo" ref={videoElementRef} />
       </div>
-      <p className="scannedText">SCANNED: {userHash}</p>
+      <p className="scannedText">사용자 정보: {userId}</p>
     </div>
   );
 };
