f33c8e2353e2c46902899fc7a8f2b63bb2eb0f69
komg00 <komg00@naver.com>
2024-05-10 20:13:21 +0900

    Add file

---
+++ src/components/QRScanner.js
@@ -0,0 +1,43 @@
+import React from "react";
+import QrScanner from "qr-scanner";
+import { useEffect, useRef, useState } from "react";
+
+const QRScanner = (props) => {
+  const videoElementRef = useRef(null);
+  const [userHash, setUserHash] = useState("");
+
+  useEffect(() => {
+    const video = videoElementRef.current;
+    const qrScanner = new QrScanner(
+      video,
+      (result) => {
+        console.log("decoded qr code:", result);
+        setUserHash(result.data);
+      },
+      {
+        returnDetailedScanResult: true,
+        highlightScanRegion: true,
+        highlightCodeOutline: true,
+      }
+    );
+    qrScanner.start();
+    console.log("start");
+
+    return () => {
+      console.log(qrScanner);
+      qrScanner.stop();
+      qrScanner.destroy();
+    };
+  }, []);
+
+  return (
+    <div>
+      <div className="videoWrapper">
+        <video className="qrVideo" ref={videoElementRef} />
+      </div>
+      <p className="scannedText">SCANNED: {userHash}</p>
+    </div>
+  );
+};
+
+export default QRScanner;
